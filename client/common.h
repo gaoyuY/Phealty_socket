af8f0cb0bb9d914496c9931d23dc7c3465614f29
gaoyu <2282940822@qq.com>
2018-12-26 16:06:45 +0800

    add a code

---
+++ client/common.h
@@ -0,0 +1,36 @@
+/*************************************************************************
+	> File Name: common.h
+	> Author:gaoyu
+	> Mail:2282940822 
+	> Created Time: 2018年10月18日 星期四 16时41分05秒
+ ************************************************************************/
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+#include<arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>//线程函数
+#include <sys/types.h>
+#include <sys/stat.h>
+#define MAX_SIZE 2048 
+#define INS 7
+#endif
